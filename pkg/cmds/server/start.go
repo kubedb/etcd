a096969ddedb327864248247e04749fa6029fff2
Tamal Saha <tamal@appscode.com>
2018-05-03 14:38:24 -0700

    Update client-go to 7.0.0 (#2)

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -56,7 +56,7 @@ func (o EtcdServerOptions) Config() (*server.EtcdServerConfig, error) {
 	}
 
 	serverConfig := genericapiserver.NewRecommendedConfig(server.Codecs)
-	if err := o.RecommendedOptions.ApplyTo(serverConfig); err != nil {
+	if err := o.RecommendedOptions.ApplyTo(serverConfig, server.Scheme); err != nil {
 		return nil, err
 	}
 	serverConfig.EnableMetrics = true
