887d2e1f41d45736bb6b38d4556c1cb746705c1e
the-redback <maruf.2hin@gmail.com>
2018-09-20 16:20:44 +0600

    Support Termination Policy. xref: https://github.com/kubedb/project/issues/182

--- pkg/cmds/server/options.go
+++ pkg/cmds/server/options.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"flag"
-	"k8s.io/client-go/dynamic"
 	"time"
 
 	"github.com/appscode/kutil/meta"
@@ -15,6 +14,7 @@ import (
 	"github.com/spf13/pflag"
 	core "k8s.io/api/core/v1"
 	kext_cs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 )
